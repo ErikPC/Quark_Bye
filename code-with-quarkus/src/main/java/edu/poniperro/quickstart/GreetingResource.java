d7d0301e3944aa293196c5a7283460cfcf751040
ErikPC <epardillo@cifpfbmoll.eu>
2022-04-08 12:03:58 +0200

    feat: añadir CofginProperty para usar parametro

--- code-with-quarkus/src/main/java/edu/poniperro/quickstart/GreetingResource.java
+++ code-with-quarkus/src/main/java/edu/poniperro/quickstart/GreetingResource.java
@@ -5,16 +5,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/bye")
 public class GreetingResource {
+
+    @ConfigProperty(name = "greetings.inicio")
+    String msg;
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String hello() {
-        return "<img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F2img.net%2Fh%2F24.media.tumblr.com%2Ftumblr_lmjqalqtmX1qah4nko1_500.gif&f=1&nofb=1'%>"
-                +
-                "<h1>Bienvenido</h1>" +
-                "<h2>Informo que va a estar poblado de cosas de One Piece</h2>";
+        return msg;
     }
 
 }
