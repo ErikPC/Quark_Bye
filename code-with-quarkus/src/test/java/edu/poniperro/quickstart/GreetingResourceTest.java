1c6c98d58fe73c9cb803adba25096b076a3367ef
ErikPC <epardillo@cifpfbmoll.eu>
2022-05-03 18:29:00 +0200

    test: crear test de ruta /bye

--- code-with-quarkus/src/test/java/edu/poniperro/quickstart/GreetingResourceTest.java
+++ code-with-quarkus/src/test/java/edu/poniperro/quickstart/GreetingResourceTest.java
@@ -12,10 +12,11 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/C:/Program Files/Git/bye")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .when().get("/bye")
+                .then()
+                .statusCode(200)
+                .body(is(
+                        "<img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F2img.net%2Fh%2F24.media.tumblr.com%2Ftumblr_lmjqalqtmX1qah4nko1_500.gif&f=1&nofb=1'%><h1>Bienvenido</h1><h2>Informo que va a estar poblado de cosas de One Piece</h2>"));
     }
 
 }
