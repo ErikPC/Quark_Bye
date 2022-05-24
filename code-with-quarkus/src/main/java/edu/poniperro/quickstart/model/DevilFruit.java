4f529d7c0b9db4ea88eebea2fd4558466716257c
ErikPC <epardillo@cifpfbmoll.eu>
2022-05-24 15:43:21 +0200

    feat: crear modelo de json

---
+++ code-with-quarkus/src/main/java/edu/poniperro/quickstart/model/DevilFruit.java
@@ -0,0 +1,15 @@
+package edu.poniperro.quickstart.model;
+
+public class DevilFruit {
+
+    public String name;
+    public String description;
+
+    public DevilFruit() {
+    }
+
+    public DevilFruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
