680f4f4f4e47649d0bccae6a5487e91e1fc3bd4f
ErikPC <epardillo@cifpfbmoll.eu>
2022-05-26 20:43:00 +0200

    test: crear test

--- code-with-quarkus/src/test/java/edu/poniperro/quickstart/TestDevilFruits.java
+++ code-with-quarkus/src/test/java/edu/poniperro/quickstart/TestDevilFruits.java
@@ -1,25 +1,14 @@
 package edu.poniperro.quickstart;
 
-import org.jboss.resteasy.annotations.Body;
 import org.junit.jupiter.api.Test;
 
-import edu.poniperro.quickstart.model.DevilFruit;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Set;
-
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.response.Response;
 
 @QuarkusTest
 public class TestDevilFruits {
-
-    private Set<DevilFruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
 
     @Test
     public void testGet() {
