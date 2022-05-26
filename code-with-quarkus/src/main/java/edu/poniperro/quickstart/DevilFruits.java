8eb47d8f4a2bc34e47709ed464845fd34035260f
ErikPC <epardillo@cifpfbmoll.eu>
2022-05-26 13:48:56 +0200

    feat: añadir atributo berris y etiquetas validacion

--- code-with-quarkus/src/main/java/edu/poniperro/quickstart/DevilFruits.java
+++ code-with-quarkus/src/main/java/edu/poniperro/quickstart/DevilFruits.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -20,8 +21,8 @@ public class DevilFruits {
     private Set<DevilFruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
 
     public DevilFruits() {
-        fruits.add(new DevilFruit("Gomu-Gomu", "Paramecia"));
-        fruits.add(new DevilFruit("Yami-Yami", "Logia"));
+        fruits.add(new DevilFruit("Gomu-Gomu", "Paramecia", 1000000000));
+        fruits.add(new DevilFruit("Yami-Yami", "Logia", 1000000000));
     }
 
     @GET
@@ -31,7 +32,7 @@ public class DevilFruits {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Set<DevilFruit> add(DevilFruit fruit) {
+    public Set<DevilFruit> add(@Valid DevilFruit fruit) {
         fruits.add(fruit);
         System.out.println(fruit);
         return fruits;
--- code-with-quarkus/src/main/java/edu/poniperro/quickstart/model/DevilFruit.java
+++ code-with-quarkus/src/main/java/edu/poniperro/quickstart/model/DevilFruit.java
@@ -1,15 +1,28 @@
 package edu.poniperro.quickstart.model;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import io.smallrye.common.constraint.NotNull;
+
 public class DevilFruit {
 
+    @NotNull
     public String name;
+
+    @NotNull
     public String type;
+
+    @Min(1500000)
+    @NotNull
+    public int berris;
 
     public DevilFruit() {
     }
 
-    public DevilFruit(String name, String type) {
+    public DevilFruit(String name, String type, int berris) {
         this.name = name;
         this.type = type;
+        this.berris = berris;
     }
 }
